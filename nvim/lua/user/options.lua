9ddf2619d9b4675a75fc1446550f3704252b96f1
Raynald Escala <qwertyraynald@gmail.com>
2022-06-16 22:34:22 +0800

    revert to default behavior

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -49,5 +49,5 @@ augroup END
 ]])
 
 vim.cmd("set whichwrap+=<,>,[,],h,l")
-vim.cmd([[set iskeyword+=-]])
+-- vim.cmd([[set iskeyword+=-]])
 vim.cmd([[set formatoptions-=cro]]) -- TODO: this doesn't seem to work
