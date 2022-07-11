c6dc3150e13a5103b5901c2b55a21153fe8f92b7
Raynald Escala <qwertyraynald@gmail.com>
2022-07-11 20:07:04 +0800

    dwm: add "bar-height" patch

--- .config/suckless/dwm/config.h
+++ .config/suckless/dwm/config.h
@@ -10,6 +10,7 @@ static const unsigned int systrayiconsize = 16; /* systray icon size in px */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
+static const int user_bh            = 22;    /* 2 is the default spacing around the bar's font */
 static const int topbar             = 1;     /* 0 means bottom bar */
 static const char *fonts[]          = {	
     "JetBrains Mono Medium Nerd Font:pixelsize=14:antialias=true:autohint=true", 
