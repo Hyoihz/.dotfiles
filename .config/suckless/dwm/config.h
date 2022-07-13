7feed2f5b92f83bffe901d10800f3497a24287c8
Raynald Escala <qwertyraynald@gmail.com>
2022-07-13 16:16:04 +0800

    dwm: update underline tag offset

--- .config/suckless/dwm/config.h
+++ .config/suckless/dwm/config.h
@@ -73,7 +73,7 @@ static const int tagschemes[] = { SchemeTag1, SchemeTag2, SchemeTag3,
 
 static const unsigned int ulinepad	= 15;	/* horizontal padding between the underline and tag */
 static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
-static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
+static const unsigned int ulinevoffset	= 38;/* how far above the bottom of the bar the line should appear */
 static const int ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
 
 static const Rule rules[] = {
