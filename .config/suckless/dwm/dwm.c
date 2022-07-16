5ce2f650953076a5b23dcdf571962d58a4123d92
Raynald Escala <qwertyraynald@gmail.com>
2022-07-16 14:52:53 +0800

    dwm: change variable name of barheight

--- .config/suckless/dwm/dwm.c
+++ .config/suckless/dwm/dwm.c
@@ -2042,7 +2042,7 @@ setup(void)
 	if (!drw_fontset_create(drw, fonts, LENGTH(fonts)))
 		die("no fonts could be loaded.");
 	lrpad = drw->fonts->h;
-	bh = drw->fonts->h + user_bh;
+	bh = drw->fonts->h + barheight;
 	updategeom();
 	/* init atoms */
 	utf8string = XInternAtom(dpy, "UTF8_STRING", False);
