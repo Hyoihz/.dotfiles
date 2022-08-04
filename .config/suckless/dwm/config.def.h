2af16a8f50ae9b11cf09439be091f614c382f784
Raynald Escala <qwertyraynald@gmail.com>
2022-08-04 08:04:14 +0800

    dwm: cleanups

--- .config/suckless/dwm/config.def.h
+++ .config/suckless/dwm/config.def.h
@@ -1,57 +1,59 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 4;   /* systray spacing */
+static const unsigned int borderpx        = 3;  /* border pixel of windows */
+static const Gap default_gap              = {.isgap = 1, .realgap = 10, .gappx = 10};
+/* behavior */
+static const int topbar                   = 1;  /* 0 means bottom bar */
+static const int showbar                  = 1;  /* 0 means no bar */
+static const unsigned int snap            = 32; /* snap pixel */
+/* system tray */
+static const int showsystray              = 1;  /* 0 means no systray */
+static const int systraypinningfailfirst  = 1;  /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const unsigned int systraypinning  = 0;  /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing  = 4;  /* systray spacing */
 static const unsigned int systrayiconsize = 16; /* systray icon size in px */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
-static const int showbar            = 1;     /* 0 means no bar */
-static const int barheight          = 22;    /* 2 is the default spacing around the bar's font */
-static const int topbar             = 1;     /* 0 means bottom bar */
-static const char *fonts[]          = {	
+/* bar */
+static const int barheight                = 22; /* 2 is the default spacing around the bar's font */
+static const unsigned int colorfultag     = 1;  /* 0 means use SchemeSel for selected tag */
+/* font */
+static const char *fonts[] = {	
     "JetBrains Mono Medium Nerd Font:pixelsize=15:antialias=true:autohint=true", 
     "Material Design Icons Desktop:pixelsize=20:antialias=true:autohint=true",
     "Noto Color Emoji:pixelsize=16:antialias=true:autohint=true"
 };
-static const unsigned int colorfultag    = 1;  /* 0 means use SchemeSel for selected tag */
-static const char dmenufont[]       = "JetBrains Mono Medium Nerd Font:pixelsize=16:antialias=true:autohint=true";
-
+static const char dmenufont[]    = "JetBrains Mono Medium Nerd Font:pixelsize=16:antialias=true:autohint=true";
+/* color */
 static const char col_bg[]       = "#1E1E2E";
 static const char col_fg[]       = "#CDD6F4";
-static const char col_black[]       = "#46575A";
-static const char col_br_black[]       = "#585B70";
-static const char col_red[]       = "#F38BA8";
-static const char col_green[]       = "#A6E3A1";
-static const char col_yellow[]       = "#F9E2AF";
-static const char col_blue[]       = "#89B4FA";
-static const char col_magenta[]       = "#F5C2E7";
-static const char col_cyan[]       = "#94E2D5";
-static const char col_white[]       = "#BAC2DE";
-static const char col_br_white[]       = "#A6ADC8";
+static const char col_black[]    = "#46575A";
+static const char col_br_black[] = "#585B70";
+static const char col_red[]      = "#F38BA8";
+static const char col_green[]    = "#A6E3A1";
+static const char col_yellow[]   = "#F9E2AF";
+static const char col_blue[]     = "#89B4FA";
+static const char col_magenta[]  = "#F5C2E7";
+static const char col_cyan[]     = "#94E2D5";
+static const char col_white[]    = "#BAC2DE";
+static const char col_br_white[] = "#A6ADC8";
 
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm]  = { col_fg, col_bg, col_black },
-	[SchemeSel]   = { col_fg, col_bg, col_red },
-	[SchemeCol1]  = { col_yellow,      col_bg, col_black },
-	[SchemeCol2]  = { col_blue,      col_bg, col_black },
-	[SchemeCol3]  = { col_cyan,      col_bg, col_black },
-	[SchemeCol4]  = { col_magenta,      col_bg, col_black },
-	[SchemeCol5]  = { col_green,      col_bg, col_black },
-	[SchemeCol6]  = { col_red,      col_bg, col_black },
-	[SchemeTag]   = { col_br_black, col_bg, col_black },
+	/*                 fg            bg      border   */
+	[SchemeNorm]   = { col_fg,       col_bg, col_black }, /* \x0b */
+	[SchemeSel]    = { col_fg,       col_bg, col_red   },
+	[SchemeTag]    = { col_br_black, col_bg, col_black },
 	[SchemeTag1]   = { col_br_white, col_bg, col_black },
-	[SchemeTag2]   = { col_yellow, col_bg, col_black },
-	[SchemeTag3]   = { col_green, col_bg, col_black },
-	[SchemeTag4]   = { col_cyan, col_bg, col_black },
-	[SchemeTag5]   = { col_blue, col_bg, col_black },
-	[SchemeTag6]   = { col_magenta, col_bg, col_black },
-	[SchemeTag7]   = { col_red, col_bg, col_black },
-	[SchemeLayout]   = { col_white, col_bg, col_black },
+	[SchemeTag2]   = { col_yellow,   col_bg, col_black },
+	[SchemeTag3]   = { col_green,    col_bg, col_black },
+	[SchemeTag4]   = { col_cyan,     col_bg, col_black },
+	[SchemeTag5]   = { col_blue,     col_bg, col_black },
+	[SchemeTag6]   = { col_magenta,  col_bg, col_black },
+	[SchemeTag7]   = { col_red,      col_bg, col_black },
+	[SchemeLayout] = { col_white,    col_bg, col_black },
+	[SchemeCol1]   = { col_yellow,   col_bg, col_black }, /* \x0c */
+	[SchemeCol2]   = { col_blue,     col_bg, col_black }, /* \x0d */
+	[SchemeCol3]   = { col_cyan,     col_bg, col_black }, /* \x0e */
+	[SchemeCol4]   = { col_magenta,  col_bg, col_black }, /* \x0f */
 };
 
 /* tagging */
@@ -61,10 +63,10 @@ static const int tagschemes[] = { SchemeTag1, SchemeTag2, SchemeTag3,
                                   SchemeTag4, SchemeTag5, SchemeTag6,
                                   SchemeTag7 };
 
-static const unsigned int ulinepad	= 15;	/* horizontal padding between the underline and tag */
-static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
-static const unsigned int ulinevoffset	= 38;/* how far above the bottom of the bar the line should appear */
-static const int ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
+static const unsigned int ulinepad	= 15; /* horizontal padding between the underline and tag */
+static const unsigned int ulinestroke	= 2;  /* thickness / height of the underline */
+static const unsigned int ulinevoffset	= 38; /* how far above the bottom of the bar the line should appear */
+static const int ulineall 		= 0;  /* 1 to show underline on all tags, 0 for just the active ones */
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -78,16 +80,16 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const float mfact        = 0.50; /* factor of master area size [0.05..0.95] */
+static const int nmaster        = 1;    /* number of clients in master area */
+static const int resizehints    = 0;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1;    /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[💀]",      tile },    /* first entry is default */
-	{ "[👻]",      NULL },    /* no layout function means floating behavior */
-	{ "👽",      monocle },
+	{ "[💀]",     tile }, /* first entry is default */
+	{ "[👻]",     NULL }, /* no layout function means floating behavior */
+	{ "[M]",      monocle },
 };
 
 /* key definitions */
@@ -102,10 +104,21 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-i", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_fg, "-sb", col_br_black, "-sf", col_blue, "-nhb", col_bg, "-nhf", col_yellow, "-shb", col_br_black, "-shf", col_green, "-h", "41", NULL };
+static const char *dmenucmd[] = {
+	"dmenu_run",
+	"-m", dmenumon,
+	"-fn", dmenufont,
+	"-nb", col_bg,
+	"-nf", col_fg,
+	"-sb", col_br_black,
+	"-sf", col_blue,
+	"-nhb", col_bg,
+	"-nhf", col_yellow,
+	"-shb", col_br_black,
+	"-shf", col_green,
+	"-h", "41", "-i", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
@@ -174,7 +187,7 @@ static Button buttons[] = {
 	{ ClkStatusText,        0,              Button1,        sigdwmblocks,   {.i = 1} },
 	{ ClkStatusText,        0,              Button2,        sigdwmblocks,   {.i = 2} },
 	{ ClkStatusText,        0,              Button3,        sigdwmblocks,   {.i = 3} },
-    { ClkStatusText,        0,              Button4,        sigdwmblocks,   {.i = 4} },
+        { ClkStatusText,        0,              Button4,        sigdwmblocks,   {.i = 4} },
 	{ ClkStatusText,        0,              Button5,        sigdwmblocks,   {.i = 5} },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
