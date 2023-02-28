b59afc1693cccb263aa3efb1155a49cf743de3bf
Raynald Escala <qwertyraynald@gmail.com>
2023-02-28 17:18:17 +0800

    dmenu: remove set config since dwm handles it

--- .config/suckless/dmenu/config.h
+++ .config/suckless/dmenu/config.h
@@ -5,9 +5,8 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-    "JetBrains Mono Nerd Font:style=medium:pixelsize=16:antialias=true:autohint=true"
+	"monospace:size=10"
 };
-
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
 static const unsigned int baralpha = 0xd0;
@@ -24,11 +23,11 @@ static const unsigned int alphas[][3]      = {
 
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-    [SchemeNorm] = { "#cdd6f4", "#1e1e2e" },
-	[SchemeSel] = { "#1e1e2e", "#89dceb" },
-	[SchemeOut] = { "#000000", "#89dceb" },
-	[SchemeNormHighlight] = { "#F9E2AF", "#1E1E2E" },
-	[SchemeSelHighlight] = { "#A6E3A1", "#585B70" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
