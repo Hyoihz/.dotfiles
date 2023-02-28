4c59a59332ac033b3af8656d30752dc53ee93f20
Raynald Escala <qwertyraynald@gmail.com>
2023-02-28 16:24:30 +0800

    dwm: add "swallow" patch

--- .config/suckless/dwm/config.h
+++ .config/suckless/dwm/config.h
@@ -6,6 +6,7 @@ static const Gap default_gap              = {.isgap = 1, .realgap = 10, .gappx =
 /* behavior */
 static const int topbar                   = 1;  /* 0 means bottom bar */
 static const int showbar                  = 1;  /* 0 means no bar */
+static const int swallowfloating          = 0;  /* 1 means swallow floating windows by default */
 static const unsigned int snap            = 32; /* snap pixel */
 /* system tray */
 static const int showsystray              = 1;  /* 0 means no systray */
@@ -96,10 +97,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "obs",      NULL,       NULL,       1 << 3,       0,           -1 },
+	/* class            instance    title               tags mask     isfloating     isterminal     noswallow     monitor */
+	{ "Gimp",           NULL,       NULL,               0,            1,             0,             0,            -1 },
+	{ "firefox",        NULL,       NULL,               1 << 1,       0,             0,             -1,           -1 },
+	{ "obs",            NULL,       NULL,               1 << 3,       0,             0,             0,            -1 },
+	{ "st-256color",    NULL,       NULL,               0,            0,             1,             0,            -1 },
+	{ NULL,             NULL,       "Event Tester",     0,            0,             0,             1,            -1 }, /* xev */
 };
 
 /* layout(s) */
